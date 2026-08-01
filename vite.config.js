748691a267b6d547a443fdf4c2c012d0823b3fe9
Gerugo <gerugo@gmail.com>
2026-08-01 10:37:02 +0000

    pwa2

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'autoUpdate', // Actualiza la app automáticamente en los móviles cuando subas cambios
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Clínica Roque - Turnos',
         short_name: 'Turnos',
         description: 'Gestión inteligente de sala de espera',
-        theme_color: '#1e293b',
+        theme_color: '#000000', // Pon aquí el color principal de tu UI
         background_color: '#ffffff',
-        display: 'standalone',
+        display: 'standalone', // ESTO ES CLAVE: Oculta la barra de direcciones del navegador
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
